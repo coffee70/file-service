5450998e18ab3b5cb6eba9efc620a48f9dbfda64
Evan <ehaeick2@gmail.com>
2024-07-27 19:07:45 -0400

    cors policy to talk to manager

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const cors = require('cors')
+require('dotenv').config();
+
+const corsOptions = {
+    origin: process.env.MANAGER_URL,
+    optionsSuccessStatus: 200
+}
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function(_req, _file, cb) {
         cb(null, 'uploads/');
     },
-    filename: function(req, file, cb) {
+    filename: function(_req, file, cb) {
         cb(null, file.originalname);
     }
 });
@@ -17,7 +24,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Serve static files from the "uploads" directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', cors(corsOptions), express.static(path.join(__dirname, 'uploads')));
 
 app.post('/upload', upload.single('file'), (req, res) => {
     const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.originalname}`;
