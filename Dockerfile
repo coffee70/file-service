d880cdd98838bafd2d5b6a1fd3c971ae002d8189
Evan <ehaeick2@gmail.com>
2024-11-21 13:47:36 -0500

    allow for file deletion

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ COPY . .
 
 EXPOSE 9000
 
-CMD ["npm", "start"]
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors')
+const fs = require('fs');
 require('dotenv').config();
 
 const corsOptions = {
@@ -11,6 +12,12 @@ const corsOptions = {
 
 const app = express();
 const PORT = process.env.PORT || 9000;
+
+// Middleware to log incoming requests
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} request for ${req.url}`);
+    next();
+});
 
 const storage = multer.diskStorage({
     destination: function(_req, _file, cb) {
@@ -28,7 +35,20 @@ app.use('/uploads', cors(corsOptions), express.static(path.join(__dirname, 'uplo
 
 app.post('/upload', upload.single('file'), (req, res) => {
     const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.originalname}`;
+    console.log(`File uploaded: ${req.file.originalname}`);
     res.json({ filename: req.file.originalname, url: fileUrl });
+});
+
+app.delete('/delete/:filename', (req, res) => {
+    const filePath = path.join(__dirname, 'uploads', req.params.filename);
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error(`Error deleting file: ${req.params.filename}`);
+            return res.status(500).json({ error: 'File not found or could not be deleted' });
+        }
+        console.log(`File deleted: ${req.params.filename}`);
+        res.status(200).json({ message: 'File deleted successfully' });
+    });
 });
 
 app.listen(PORT, () => {
